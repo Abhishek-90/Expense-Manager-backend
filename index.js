2f18f6b6b9b44fbbd625bfa898ef21610f069134
Abhishek-90 <holaniabhishek90@gmail.com>
2021-12-30 10:58:35 +0530

    Changed Start script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const {connectToMongoose} = require('./db');
 const app = express()
-const port = 5000
+const port = 9000
 
 app.use(express.json());
 app.use(cors());

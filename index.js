936c77173bcbc8b50b3cdd896eff482e93383fa6
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-02 20:11:36 +0530

    Allowed CORS

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 connectToMongoose();
 
 app.use('/auth',authRoutes.router);

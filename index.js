013c41a5b42173d51e947eb95efdfe63ecef2f75
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-02 20:10:01 +0530

    Changed Ports

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import * as authRoutes from './routes/auth.js';
 import * as transactionRoutes from './routes/transaction.js'
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cors());
@@ -14,6 +14,6 @@ connectToMongoose();
 app.use('/auth',authRoutes.router);
 app.use('/transaction',transactionRoutes.router);
 
-app.listen(process.env.PORT || port, () => {
+app.listen( port, () => {
   console.log(`Expense Tracker listening at http://localhost:${port}`)
 })

906e55767a827f2c1da685bc2a945b0cbc57a698
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-03 13:22:56 +0530

    Changed cors

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ import * as authRoutes from './routes/auth.js';
 import * as transactionRoutes from './routes/transaction.js'
 
 const app = express()
-const port = process.env.PORT || 5000
-
+app.use(cors());
 app.use(express.json());
-app.use(cors({
-  origin:'*'
-}));
-
 connectToMongoose();
+
+const port = process.env.PORT || 5000
 
 app.use('/auth',authRoutes.router);
 app.use('/transaction',transactionRoutes.router);

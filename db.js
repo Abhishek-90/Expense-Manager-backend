0f11fb18c0801bcd9333a0fc36e0f47b23641aca
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-03 19:51:15 +0530

    Try/catch to connectToMongoose

--- db.js
+++ db.js
@@ -6,13 +6,18 @@ dotenv.config();
 const dbUri = "mongodb+srv://expensemanager:expensemanager@cluster0.ifb0d.mongodb.net/ExpenseManager?retryWrites=true&w=majority";
 
 const connectToMongoose = () => {
-    mongoose.connect(
-        dbUri,
-        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-        () => {
-          console.log('Connected to MongoDB');
-        }
-      );
+    try {
+        mongoose.connect(
+            dbUri,
+            { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+            () => {
+              console.log('Connected to MongoDB');
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 export { connectToMongoose };

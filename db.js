78e7f06ea0747e1926549524705df931a56fec71
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-10 19:02:21 +0530

    Changed Mongoose

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-// import mongoose from "mongoose";
-import { MongoClient } from 'mongodb'
+import mongoose from "mongoose";
+// import { mongoose } from 'mongodb'
 import dotenv from 'dotenv';
 dotenv.config();
 
 const dbUri = "mongodb+srv://expensemanager:expensemanager@cluster0.ifb0d.mongodb.net/ExpenseManager?retryWrites=true&w=majority";
 
+mongoose.set('bufferCommands', false);
+
 const connectToMongoose = () => {
     try {
-        MongoClient.connect(
+        mongoose.connect(
             dbUri,
             { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
             () => {

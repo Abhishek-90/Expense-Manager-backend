eeb8ba0d80e104b383b5d02385c38b3c43bb3a0f
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-10 16:01:25 +0530

    Change Headers to All:

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
   next();
 })

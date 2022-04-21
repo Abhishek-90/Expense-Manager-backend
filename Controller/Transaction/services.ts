55e482a3305b44e0bb1d4d79e9347721e2a5cdc0
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-21 10:45:45 +0530

    Added services for Transaction

--- Controller/Transaction/services.ts
+++ Controller/Transaction/services.ts
@@ -19,3 +19,59 @@ export const addTransaction =  async (req:typeof request,res:typeof response)=>{
       return res.send(400).json({error});
   }
 }
+
+export const getStatement = async (req:typeof request,res:typeof response)=>{
+    try{
+        const statement = await transaction.find({
+            email:req.body.email,
+        });
+        
+        return res.status(200).send({'statement':statement, status:'success'});
+    }
+    catch(error){
+        return res.status(400).json({error});
+    }
+}
+
+export const removeTransaction = async (req:typeof request,res:typeof response)=>{
+    try{
+        
+        if(req.body.email !== null){
+            const response = await transaction.findByIdAndRemove(req.body.id);
+            return res.status(200).send({status:'success'});
+        }
+        else{
+            return res.status(404).send({"Message":"Unauthorised Access", status:'fail'});
+        }
+    }
+    catch(error){
+        return res.status(400).send({status: 'fail'});
+    }
+}
+
+export const updateTransactionDetails = async (req:typeof request,res:typeof response) =>{
+    try{
+
+        const exists = await transaction.findById(req.body.id);
+
+        if(!exists)
+            return res.send(400).send({Message: 'Transaction Does Not Exists', status: 'success'});
+        
+        if(req.body.email !== null){
+            const response = await transaction.findByIdAndUpdate(req.body.id ,
+                {
+                    type: req.body.type,
+                    description: req.body.description,
+                    amount: req.body.amount,
+                    tag: req.body.tag
+            })
+            return res.status(200).send({status:'success'});
+        }
+        else{
+            return res.status(404).send({"Message":"Unauthorised Access", status: 'fail'});
+        }
+    }
+    catch(error){
+        return res.status(400).json({error});
+    }
+}
--- Controller/Transaction/transaction.ts
+++ Controller/Transaction/transaction.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
 import {fetchUser} from '../../middleware/fetchUser';
 const router = express.Router();
-import transaction from '../../models/transactionModel';
 import * as service from './services';
 
 //ROUTE 1: API Enpoint to add about the transactions the User Makes. Login Required.
@@ -13,67 +11,20 @@ service.addTransaction
 
 //ROUTE 2: API Enpoint to get transaction statement of the user. Login Required.
 router.get('/statement',
-fetchUser
-,async (req,res)=>{
-    try{
-        const statement = await transaction.find({
-            email:req.body.email,
-        });
-        
-        return res.status(200).send({'statement':statement, status:'success'});
-    }
-    catch(error){
-        return res.status(400).json({error});
-    }
-})
+fetchUser,
+service.getStatement
+)
 
 //ROUTE 3: API Enpoint to Delete a transaction of the user. Login Required.
 router.delete('/remove',
-fetchUser
-,async (req,res)=>{
-    try{
-        
-        if(req.body.email !== null){
-            const response = await transaction.findByIdAndRemove(req.body.id);
-            return res.status(200).send({status:'success'});
-        }
-        else{
-            return res.status(404).send({"Message":"Unauthorised Access", status:'fail'});
-        }
-    }
-    catch(error){
-        return res.status(400).send({status: 'fail'});
-    }
-})
+fetchUser,
+service.removeTransaction
+)
 
 //ROUTE 4: API Enpoint to Update transaction information of the user. Login Required.
 router.put('/update',
-fetchUser
-,async (req,res)=>{
-    try{
-
-        const exists = await transaction.findById(req.body.id);
-
-        if(!exists)
-            return res.send(400).send({Message: 'Transaction Does Not Exists', status: 'success'});
-        
-        if(req.body.email !== null){
-            const response = await transaction.findByIdAndUpdate(req.body.id ,
-                {
-                    type: req.body.type,
-                    description: req.body.description,
-                    amount: req.body.amount,
-                    tag: req.body.tag
-            })
-            return res.status(200).send({status:'success'});
-        }
-        else{
-            return res.status(404).send({"Message":"Unauthorised Access", status: 'fail'});
-        }
-    }
-    catch(error){
-        return res.status(400).json({error});
-    }
-})
+fetchUser,
+service.updateTransactionDetails
+)
 
 export {router};

8517a24d40a6c96d22ddb6ba58eeb8f9a05aa25d
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-21 11:00:07 +0530

    Added services for Authentication

--- Controller/Auth/auth.ts
+++ Controller/Auth/auth.ts
@@ -1,88 +1,23 @@
 import express from "express"
-import { body, validationResult } from "express-validator"
 const router = express.Router()
-import bcrypt from 'bcrypt'
-import user from '../../models/user'
-import jsonwebtoken from 'jsonwebtoken'
-import { encryptionKey } from "../../Constants/constants"
-import * as status from '../../Constants/Status'
+import * as service from './services'
+import { body } from "express-validator"
 
 //ROUTE 1: API Endpoint for new User Registration. No Login Required.
 router.post('/signup',
     [
         body('email').isEmail().withMessage("Enter Valid Email Address."),
         body('password').isLength({min: 8}).withMessage("Minimum 8 characters Password")
-    ]
-,async (req:express.Request,res:express.Response)=>{
-    // Storing errors in input data inside errors
-    const errors = validationResult(req);
-
-    try{
-        if(!errors.isEmpty()){
-            //If there are errors in the data sent, don't proceed further.
-            return res.status(status.BADREQUEST).send({errors});
-        }
-        //Checking if Email address entered by user while sign up is laready associated with another Profile.
-        const exists = await user.find({
-            email: req.body.email
-        });
-
-        if(exists.length !== 0){
-            //Email address already associated with another ID.
-            return res.status(status.BADREQUEST).json({"Message":"User Id Already Taken.", 'status':'fail'});
-        }
-        
-        //Encrypting Password before storing it into database
-        const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
-
-        const response = await user.create({
-            name:req.body.name,
-            email: req.body.email,
-            password: secPass
-        }).then().catch((e) => console.log(e));
-
-        console.log("Got Response")
-
-        //Generating auth token to be sent to user.
-        const authToken = jsonwebtoken.sign({email:req.body.email},encryptionKey);
-        return res.status(status.CREATED).send({authToken});
-    }
-    catch(e){
-        return res.status(status.BADREQUEST).json({error:e});
-    }
-})
+    ],
+    service.signup
+)
 
 //ROUTE 2: API Endpoint for existing users to login. No Login Required.
 router.post('/login',
     [
         body('email').isEmail().withMessage("Enter Valid Email Address.")
-    ]
-,async (req:express.Request,res:express.Response)=>{
-    // Storing errors in input data inside errors
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        //Email address already associated with another ID. 
-        return res.status(status.BADREQUEST).send({errors});
-    }
-
-    const response = await user.findOne({
-        email: req.body.email,
-    });
-
-    if(response === null)
-        return res.send(status.BADREQUEST).json({"Message":"Invalid Credentials"});
-
-    const passwordValidation:boolean = await bcrypt.compare(req.body.password, response.password);
-
-    if(passwordValidation){
-        const authToken = jsonwebtoken.sign({email:req.body.email},encryptionKey);
-        return res.send(status.OK).json({'authToken':authToken, 'status' :'success'});
-
-    }else{
-        return res.send(status.NOTFOUND).json({Message: "Invalid Credentials",status:'fail'});
-    }
-})
+    ],
+    service.login
+)
 
 export { router };
---
+++ Controller/Auth/services.ts
@@ -0,0 +1,74 @@
+import * as status from '../../Constants/Status'
+import express from "express"
+import { validationResult } from "express-validator"
+import user from '../../models/user'
+import bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken'
+import { encryptionKey } from "../../Constants/constants"
+
+export const signup = async (req:express.Request,res:express.Response)=>{
+  // Storing errors in input data inside errors
+  const errors = validationResult(req);
+
+  try{
+      if(!errors.isEmpty()){
+          //If there are errors in the data sent, don't proceed further.
+          return res.status(status.BADREQUEST).send({errors});
+      }
+      //Checking if Email address entered by user while sign up is laready associated with another Profile.
+      const exists = await user.find({
+          email: req.body.email
+      });
+
+      if(exists.length !== 0){
+          //Email address already associated with another ID.
+          return res.status(status.BADREQUEST).json({"Message":"User Id Already Taken.", 'status':'fail'});
+      }
+      
+      //Encrypting Password before storing it into database
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      const response = await user.create({
+          name:req.body.name,
+          email: req.body.email,
+          password: secPass
+      }).then().catch((e) => console.log(e));
+
+      console.log("Got Response")
+
+      //Generating auth token to be sent to user.
+      const authToken = jsonwebtoken.sign({email:req.body.email},encryptionKey);
+      return res.status(status.CREATED).send({authToken});
+  }
+  catch(e){
+      return res.status(status.BADREQUEST).json({error:e});
+  }
+}
+
+export const login = async (req:express.Request,res:express.Response)=>{
+  // Storing errors in input data inside errors
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+      //Email address already associated with another ID. 
+      return res.status(status.BADREQUEST).send({errors});
+  }
+
+  const response = await user.findOne({
+      email: req.body.email,
+  });
+
+  if(response === null)
+      return res.send(status.BADREQUEST).json({"Message":"Invalid Credentials"});
+
+  const passwordValidation:boolean = await bcrypt.compare(req.body.password, response.password);
+
+  if(passwordValidation){
+      const authToken = jsonwebtoken.sign({email:req.body.email},encryptionKey);
+      return res.json({'authToken':authToken, 'status' :'success'});
+
+  }else{
+      return res.send(status.NOTFOUND).json({Message: "Invalid Credentials",status:'fail'});
+  }
+}

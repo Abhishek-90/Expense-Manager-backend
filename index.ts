5873cd9906162e1d79b54b1d7877de79f75bc5cc
Abhishek-90 <holaniabhishek90@gmail.com>
2022-10-18 19:18:49 +0530

    Clean up

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
 import express from "express";
-import cors from 'cors';
-import { connectToMongoose } from './db';
-import * as authRoutes from './Controller/Auth/auth';
-import * as transactionRoutes from './Controller/Transaction/transaction'
+import cors from "cors";
+import { connectToMongoose } from "./db";
+import * as authRoutes from "./Controller/Auth/auth";
+import * as transactionRoutes from "./Controller/Transaction/transaction";
 
 const app = express();
 
-app.use(cors({
-  origin:"http://localhost:3000",
-  // allowedHeaders:'Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 connectToMongoose();
 
-const port = process.env.PORT || 5000
+const port = 5000;
 
-app.use('/auth',authRoutes.router);
-app.use('/transaction',transactionRoutes.router);
+app.use("/auth", authRoutes.router);
+app.use("/transaction", transactionRoutes.router);
 
-app.listen( port, () => {
-  console.log(`Expense Tracker listening at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Expense Tracker listening at http://localhost:${port}`);
+});

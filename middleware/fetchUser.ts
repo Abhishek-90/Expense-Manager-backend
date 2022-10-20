dd5afbba883f0f71b9c55923e9cff152f5f0d8b6
Abhishek-90 <holaniabhishek90@gmail.com>
2022-10-20 12:54:39 +0530

    Cookie Parser Added for token extraction

--- middleware/fetchUser.ts
+++ middleware/fetchUser.ts
@@ -9,17 +9,29 @@ export const fetchUser = (
   next: NextFunction
 ) => {
   try {
-    const token = req.body.authToken;
-    console.log(req.headers.cookie);
-
-    if (!token) {
+    const cookie = req.headers.cookie;
+    if (!cookie) {
       return res.status(status.UNAUTHORIZED).json({ Message: "Login required" });
     }
 
-    const data = jsonwebtoken.verify(token, encryptionKey) as { email: string };
+    const cookieObject = customCookieParser(req.headers.cookie);
+    console.log(cookieObject["authToken"]);
+    const data = jsonwebtoken.verify(cookieObject["authToken"], encryptionKey) as { email: string };
     req.body.email = data.email;
     next();
   } catch (error: any) {
     return res.status(status.BADREQUEST).json({ error });
   }
 };
+
+const customCookieParser = (cookie:string|undefined):any => {
+  const cookieArray = cookie?.split(";");
+  let cookieObject = {};
+  cookieArray?.forEach(cookie => {
+    const key = cookie.split("=")[0];
+    const value = cookie.split("=")[1];
+    cookieObject = {...cookieObject, [key]:value};
+  })
+
+  return cookieObject;
+}

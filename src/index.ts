41f4eb27813cbd0a1f8317115b11b5b186ad0333
Abhishek-90 <holaniabhishek90@gmail.com>
2022-10-29 13:43:04 +0530

    vercel deploy

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://animated-dasik-8cb380.netlify.app/"],
     credentials: true,
   })
 );

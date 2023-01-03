cb9afd17570d4abd731a4cc0aaab26a2ce631f8d
Abhishek-90 <holaniabhishek90@gmail.com>
2023-01-03 19:22:04 +0530

    added constants

--- Shared/Constants/TransactionConstants.ts
+++ Shared/Constants/TransactionConstants.ts
@@ -1,2 +1,12 @@
+// Transaction type constants.
 export const EXPENSE: string = "expense";
 export const INCOME: string = "income";
+
+// Transaction tags.
+export const FOOD: string = "food";
+export const MEDICAL: string = "medical";
+export const ENTERTAINMENT: string = "entertainment";
+export const INVESTMENT: string = "investment";
+export const LUXURY: string = "luxury";
+export const TRAVEL: string = "travel";
+export const CLOTHING: string = "clothing";

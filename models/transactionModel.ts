9571c0c9f0ee2bb864e329552afb4b4ae97d7c52
Abhishek-90 <holaniabhishek90@gmail.com>
2023-01-03 19:22:10 +0530

    minor change

--- models/transactionModel.ts
+++ models/transactionModel.ts
@@ -1,31 +1,31 @@
-import mongoose from 'mongoose';
-const {Schema}  = mongoose;
-import user from './user';
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+import user from "./user";
 
 const transactionSchema = new Schema({
-    email:{
-        type: String,
-        ref: user
-    },
-    date:{
-        type: String
-    },
-    type:{
-        type: String,
-        required: true
-    },
-    amount:{
-        type: String,
-        required: true
-    },
-    description:{
-        type: String,
-        required: true,
-    },
-    tag:{
-        type: String,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    ref: user,
+  },
+  date: {
+    type: String,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+  },
+});
 
 export default mongoose.model("transaction", transactionSchema);
